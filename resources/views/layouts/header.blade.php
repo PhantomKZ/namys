87e1d23d802edbc25935614671b866a566663dcd
Eugene <djon.ilchenko2002@gmail.com>
2025-04-05 03:01:52 +0500

    Update header & styles

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,13 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-main">
-        <div class="container">
-            <a class="navbar-brand" href="#"><img src="/images/main/namyslogowhite.png" alt="Логотип" width="95" height="70"></a>
+    <nav class="navbar navbar-expand-lg
+    {{ request()->routeIs('home') ? 'navbar-main' : 'navbar-light' }}">
+    <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}">
+                <img src="{{ request()->routeIs('home')
+            ? asset('images/main/namyslogowhite.png')
+            : asset('images/main/namyslogoblack.png') }}"
+                     alt="Логотип" width="95" height="70">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -18,6 +24,16 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">LOOK COLLECTION</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link auth-icon" href="{{ route('login') }}">
+                            <img src="/images/main/auth.png" alt="Авторизация" width="48" height="48">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link cart-icon" href="basket.html">
+                            <img src="/images/main/basket.png" alt="Корзина" width="48" height="48">
+                        </a>
                     </li>
                 </ul>
             </div>
